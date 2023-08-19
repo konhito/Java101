8b3d931ed908516a7a9ba0a8c76ada87b3087069
konhito <konhito02@gmail.com>
2023-08-19 18:30:30 +0530

    NEW

--- Main.java
+++ Main.java
@@ -161,5 +161,37 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         
+        // int ans = sum2();
+        // System.out.println(ans);
+        int ans = sum3(20,30);
+        System.out.println(ans);
+
+    }
+
+    static int sum3(int a,int b){
+        int sum = a + b;
+        return sum;
+
+    }
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter yout Second number:  ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;  //retuen means the functuon is over
+
+    }
+
+
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter yout Second number:  ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The Sum = "+ sum);
     }
 }
